60c2070c6eaefd7b610512dc06fae96528dde72d
Jhonny Villarroel <jvm.corp@gmail.com>
2022-02-03 08:50:21 -0400

    update clase 4

--- clase 4/clases.py
+++ clase 4/clases.py
@@ -11,6 +11,9 @@ class Coche:
     """ esta clase define el estado y comportamiento de un coche """
     # atributos de clase
     ruedas = 4
+    cart = 12
+    officio = 12
+    
     # Metodo constructor
     def __init__(self,colorAuto, aceleracion):
         # asignacion de atributos y variables
@@ -26,7 +29,6 @@ class Coche:
     def color (self, nuevoColor):
         self.__color= nuevoColor
         
-        
     # metodos de la clase
     def acelera(self):
         print("acelerando!!!!!")
@@ -34,7 +36,12 @@ class Coche:
         print(f"Frenando!!!!! con las ruedas {self.ruedas}")
 
 # Creacion de Objetos
+
 coche_rojo = Coche('rojo', '120km/h')
+
+print(f"attributo de clase -----------------> {Coche.ruedas}")
+
+
 coche_lento_azul = Coche('azul', '90km/h')
 print(f"el color de coche lento es:{coche_lento_azul.color}")
 coche_lento_azul.color="verde"
