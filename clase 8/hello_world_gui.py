b70ed614714b9268ceb80ed3383f3fd552123e5e
Jhonny Villarroel <jvm.corp@gmail.com>
2022-02-12 08:45:07 -0400

    add hello world in tkinter gui

---
+++ clase 8/hello_world_gui.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 12 08:41:12 2022
+
+@author: Diesel soft server
+"""
+
+# clase 8 /hello_world_gui.py
+
+from tkinter import *
+
+root = Tk()
+a = Label(root, text="Hello World")
+a.pack()
+root.mainloop()
--- clase 8/test_tk.py
+++ clase 8/test_tk.py
@@ -5,6 +5,8 @@ Created on Sat Feb 12 08:15:46 2022
 @author: Diesel soft server
 """
 
+# clase 8/test_tk.py
+
 import tkinter
 
 tkinter._test()
